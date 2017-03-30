7c0f9a596b10304ca5a2dd08d467af97dd613317
umesh rajashekar <umesh.rajashekar@gmail.com>
2017-03-30 10:58:46 -0400

    Tetsing out travis ci

---
+++ tests/test_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 30 10:53:36 2017
+
+@author: umesh
+"""
+
+def test_add():
+    assert 3+5 == 8
+
+def test_sub():
+    assert 3-5 == -2
